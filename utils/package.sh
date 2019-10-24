e75eccebd39386bb9dc7e7237d7cc9e6a99b31fe
Matthew King <automatthew@gmail.com>
2019-10-24 12:42:48 -0500

    utility to package up integrator creds and a smoke test

---
+++ utils/package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Zip up the smoke test script and the integrator credentials file.
+
+USAGE="$(basename $0) integrator_name path/to/secret.json"
+
+if [ $# -ne 2 ]
+  then
+    echo "usage:"
+    echo $USAGE
+    exit 1
+fi
+
+NAME=$1
+SECRETS_JSON=$2
+
+zip -j build/$NAME.zip examples/intro.sh $SECRETS_JSON
+
